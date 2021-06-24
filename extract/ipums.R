352253de742a7550c997a1da577b1b4b5a2ebf5a
mcooper <mw.coop.r@gmail.com>
2021-06-24 20:17:15 +0000

    conclude ipums

--- extract/ipums.R
+++ extract/ipums.R
@@ -1,3 +1,44 @@
+#####################################
+# Notes: 
+# Compared to DHS & MICS, IPUMS has lots of poorly coded mortality.
+# Brazil doesnt have birth month, so we excluded
+# For surveys with date of child death, in many cases, the child's death was before the birth in ~2% of cases
+# For surveys with the age of death, a large number are coded 98 for "unknown" when they died
+# So, not using IPUMS
+
+library(tidyverse)
+library(data.table)
+
+dat <- fread('~/mortalityblob/ipums/ipumsi_00004.csv') %>%
+  select(COUNTRY, YEAR, GEOLEV2, LASTBMO, LASTBYR, CHDEADYR, CHDEADMO, AGEDEADYR, 
+         AGEDEADMO, LASTBMORT, HHWT) %>%
+  merge(data.frame(COUNTRY=c(76, 332, 404, 484, 710, 800),
+                   ISO3=c("BRA", "HTI", "KEN", "MEX", "ZAF", "UGA"))) %>%
+  mutate(survey = paste0(ISO3, '-', YEAR))
+
+####### For surveys with age of death, many unknowns #######
+tab(dat$survey, case_when(dat$AGEDEADMO == 98 ~ 'unknown', 
+                          dat$AGEDEADMO == 97 ~ '< 1 yr, months unknown',
+                          dat$AGEDEADMO < 97 ~ 'known',
+                          dat$AGEDEADMO == 99 ~ 'NIU', 
+                          is.na(dat$AGEDEADYR) ~ NA_character_))
+
+####### For surveys with date of death, many unknowns, many died before born! #############
+tab(dat$survey, case_when(dat$CHDEADMO == 98 ~ 'unknown', 
+                          dat$CHDEADMO == 99 ~ 'NIU', 
+                          is.na(dat$CHDEADYR) ~ NA_character_))
+
+sel <- dat %>%
+  # Valid CHDEADMO records
+  filter(CHDEADYR < 3000, CHDEADMO < 90, LASTBMORT == '2', LASTBYR < 3000, LASTBMO < 90) %>% 
+  mutate(death_cmc = 12*(CHDEADYR - 1900) + CHDEADMO,
+         birth_cmc = 12*(LASTBYR - 1900) + LASTBMO) 
+
+
+################# SO: NOT USING IPUMS ################################
+
+# Old code:
+
 library(tidyverse)
 library(data.table)
 
@@ -6,7 +47,7 @@ dat <- fread('~/mortalityblob/ipums/ipumsi_00004.csv')
 #Only collect mortality and other data
 dat <- dat %>%
   select(COUNTRY, YEAR, GEOLEV2, LASTBMO, LASTBYR, CHDEADYR, CHDEADMO, AGEDEADYR, 
-         AGEDEADMO, LASTBMORT)
+         AGEDEADMO, LASTBMORT, HHWT)
 
 cty <- data.frame(COUNTRY=c(76, 332, 404, 484, 710, 800),
                   ISO3=c("BRA", "HTI", "KEN", "MEX", "ZAF", "UGA"))
@@ -19,20 +60,32 @@ dat <- dat[dat$ISO3 != 'BRA', ]
 ## Cant use those where mortality status of child is 'Unknown' or 'NIU'
 dat <- dat[!dat$LASTBMORT %in% c(0, 9), ]
 
-#################
-# Survey
-#################
+## Make Survey Code
 dat$survey <- paste0(dat$ISO3, '-', dat$YEAR)
 
-##################
-# Death Year
-##################
-#tab(dat$survey, case_when(dat$CHDEADYR == 0 ~ 0, dat$CHDEADYR > 2020 ~ 9999, is.na(dat$CHDEADYR) ~ NA_real_, TRUE ~ 1))
+#########################################
+### JUST DO COUNTRIES WITH CHDEAD DATA
+#########################################
+sel <- dat %>%
+  filter(survey %in% c('HTI-2003', 'KEN-1989', 'ZAF-2011', 'ZAF-2016'))
 
-dat <- dat[(dat$CHDEADYR < 3000 & dat$CHDEADMO < 90) | !is.na(dat$AGEDEADMO), ] #Remove children that are Unknown or NIU, and are missing death age data
+sel <- sel[(sel$CHDEADYR < 3000 & sel$CHDEADMO < 90), ] #Remove children that are Unknown or NIU, and are missing death age sela
+sel$death_cmc <- 12*(sel$CHDEADYR - 1900) + sel$CHDEADMO
+sel$death_cmc[sel$CHDEADYR == 0] <- NA
 
-dat$death_cmc <- 12*(dat$CHDEADYR - 1900) + dat$CHDEADMO
-dat$death_cmc[dat$CHDEADYR == 0] <- NA
+sel <- sel[sel$LASTBYR < 3000 & sel$LASTBMO < 90, ] #Remove children that are Unknown or NIU
+sel$birth_cmc <- 12*(sel$LASTBYR - 1900) + sel$LASTBMO
+
+sel <- sel %>%
+  mutate(dage = death_cmc - birth_cmc) %>%
+  filter((dage < 12) | is.na(dage))
+
+
+#########################################
+### NOW DO COUNTRIES WITH AGEDEAD DATA
+#########################################
+sel2 <- dat %>%
+  filter(
 
 ##################
 # Birth Year
@@ -49,7 +102,11 @@ dat$survey_cmc <- 12*(dat$YEAR - 1900) + 1
 #############################
 # Age at Death in Months
 #############################
-#tab(dat$survey, case_when(dat$AGEDEADMO <= 97 ~ 1, dat$AGEDEADMO > 97 ~ 99, is.na(dat$CHDEADYR) ~ NA_real_))
+tab(dat$survey, case_when(dat$AGEDEADMO == 98 ~ 'unknown', 
+                          dat$AGEDEADMO == 97 ~ '< 1 yr, months unknown',
+                          dat$AGEDEADMO < 97 ~ 'known',
+                          dat$AGEDEADMO == 99 ~ 'NIU', 
+                          is.na(dat$CHDEADYR) ~ NA_character_))
 
 dat$AGEDEADMO
 
